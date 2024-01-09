424aeb0f57e5fdea096b21f7a72bea1437889c67
AbdulRehman1891 <abdurrehman1891@gmail.com>
2024-01-10 04:09:21 +0500

    Refactored User COntroller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -17,11 +17,7 @@ const LOG_TYPE = {
   LOGOUT: "logout",
 };
 
-const LEVEL = {
-  INFO: "info",
-  ERROR: "error",
-  WARN: "warn",
-};
+
 
 const MESSAGE = {
   SIGN_IN_ATTEMPT: "User attempting to sign in",
@@ -33,6 +29,11 @@ const MESSAGE = {
   MULTIPLE_ATTEMPT_WITHOUT_VERIFY:
     "Multiple sign in attempts detected without verifying identity.",
   LOGOUT_SUCCESS: "User has logged out successfully",
+  LEVEL:{
+    INFO: "info",
+    ERROR: "error",
+    WARN: "warn",
+  }
 };
 
 const signin = async (req, res, next) => {
